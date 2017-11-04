0a1c084aca6d87845b6c6d6d847f1873bd0c1f25
Fred Souza <fredericoamsouza@gmail.com>
2017-11-04 10:39:16 -0200

    Adding empty info to overview page

---
+++ src/components/views/WalletsOverview/NoWallets.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { colors } from 'common/styles';
+
+export default () => (
+    <View style={styles.container}>
+        <Text style={styles.message}>
+            There are no wallets configured. Click on the + button to add a new one.
+        </Text>
+    </View>
+);
+
+const styles = StyleSheet.create({
+    container: {},
+    message: {
+        color: colors.black
+    }
+});
--- src/components/views/WalletsOverview/index.js
+++ src/components/views/WalletsOverview/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { HeaderIcon } from 'components/widgets';
 import { colors, measures } from 'common/styles';
+import NoWallets from './NoWallets';
 
 export class WalletsOverview extends React.Component {
 
@@ -15,10 +16,19 @@ export class WalletsOverview extends React.Component {
                 onPress={() => navigation.navigate('NewWallet')} />
         )
     });
+    
+    state = { wallets: [] };
+
+    renderBody(wallets) {
+        return (!wallets.length) ? <NoWallets /> : (
+            null
+        );
+    }
 
     render() {
         return (
             <View style={styles.background}>
+                {this.renderBody(this.state.wallets)}
             </View>
         );
     }
@@ -28,6 +38,8 @@ const styles = StyleSheet.create({
     background: {
         flex: 1,
         backgroundColor: colors.secondary,
-        padding: measures.defaultPadding
+        padding: measures.defaultPadding,
+        alignItems: 'stretch',
+        justifyContent: 'flex-start'
     }
 });
