114d9a8ec40e72b04ebdc884b87509bb14026a23
Fred Souza <fredericoamsouza@gmail.com>
2017-11-05 13:54:16 -0200

    Testing TextBullet component

---
+++ src/components/widgets/TextBullet/test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+import sinon from 'sinon';
+import { shallow } from 'enzyme';
+import { TextBullet } from './index';
+
+describe('<TextBullet />', () => {
+
+    it('should have a label `label`', () => {
+        const wrapper = shallow(<TextBullet>label</TextBullet>);
+        expect(wrapper.find(Text).children().text()).toEqual('label');
+    });
+});
