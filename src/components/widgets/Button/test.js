11477858a7c654be8d51c17c4f7a75c2da6d92ac
Fred Souza <fredericoamsouza@gmail.com>
2017-11-05 12:25:24 -0200

    Testing Button component

---
+++ src/components/widgets/Button/test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text } from 'react-native';
+import sinon from 'sinon';
+import { shallow } from 'enzyme';
+import { Button } from './index';
+
+describe('<Button />', () => {
+
+    it('should have a label `button label`', () => {
+        const wrapper = shallow(<Button>button label</Button>);
+        expect(wrapper.find(Text).children().text()).toEqual('button label');
+    });
+    
+    it('should have call the `onPress` function when clicked', () => {
+        const eventSpy = sinon.spy();
+        const wrapper = shallow(<Button onPress={eventSpy} />);
+        wrapper.simulate('press');
+        expect(eventSpy.called).toBeTruthy();
+    });
+});
