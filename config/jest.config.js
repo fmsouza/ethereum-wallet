07590e29743ed5355c4be250250ccf6cf838c202
Fred Souza <fredericoamsouza@gmail.com>
2017-11-17 01:02:08 -0200

    Moving jest config to inside a config folder

--- config/jest.config.js
+++ config/jest.config.js
@@ -5,10 +5,10 @@ const jsdom = require("jsdom").jsdom;
 
 global.document = jsdom("");
 global.window = document.defaultView;
-Object.keys(document.defaultView).forEach(property => {
+for (property in document.defaultView) {
   if (typeof global[property] === "undefined") {
     global[property] = document.defaultView[property];
   }
-});
+}
 
 enzyme.configure({ adapter: new Adapter() });
