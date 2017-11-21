b25c8767ac2866c125a1c8cf7779ab45f10ccd4b
Fred Souza <fredericoamsouza@gmail.com>
2017-11-21 00:25:16 -0200

    Showing the wallet QRcode to receive coins

--- src/components/views/ReceiveCoins/index.js
+++ src/components/views/ReceiveCoins/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { inject, observer } from 'mobx-react';
+import QRCode from 'react-native-qrcode-svg';
+import { colors, measures } from 'common/styles';
 
+@inject('wallet')
+@observer
 export class ReceiveCoins extends React.Component {
 
     render() {
+        const { wallet: { item } } = this.props;
         return (
             <View style={styles.container}>
-                <Text>Receive</Text>
+                <Text style={styles.label}>Show the code below to receive coins</Text>
+                <QRCode size={256} value={item.getAddress()} />
+                <Text style={styles.label}>{item.getAddress()}</Text>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {}
+    container: {
+        backgroundColor: colors.white,
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        padding: measures.defaultPadding
+    }
 });
