789af56b39ed0273d72ae04adadd62f88a4e4f4c
Fred Souza <fredericoamsouza@gmail.com>
2017-12-19 21:03:55 -0200

    Updating dependencies

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,14 +1,7 @@
 require("react-native-mock-render/mock");
 const enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
-const jsdom = require("jsdom").jsdom;
+const { JSDOM } = require("jsdom");
 
-global.document = jsdom("");
-global.window = document.defaultView;
-for (property in document.defaultView) {
-  if (typeof global[property] === "undefined") {
-    global[property] = document.defaultView[property];
-  }
-}
-
+global = new JSDOM("");
 enzyme.configure({ adapter: new Adapter() });
