a7d2c9cda168f45c20821ab028525b78982c6359
Fred Souza <fredericoamsouza@gmail.com>
2017-12-19 21:49:01 -0200

    Implementing the request to get the transaction history

--- src/common/services/__tests__/api.js
+++ src/common/services/__tests__/api.js
@@ -4,14 +4,40 @@ describe('ApiService', () => {
 
     it('should be able to get the price conversion between ETH, USD, BRL, and EUR', async function() {
         try {
-            const result = await Api.getPrice();
-            expect(result.status).toBe(200);
-            expect(result.data.USD).toBeDefined();
-            expect(result.data.USD).not.toBeNaN();
-            expect(result.data.EUR).toBeDefined();
-            expect(result.data.EUR).not.toBeNaN();
-            expect(result.data.BRL).toBeDefined();
-            expect(result.data.BRL).not.toBeNaN();
+            const response = await Api.getPrice();
+            expect(response.status).toBe(200);
+            expect(response.data.USD).toBeDefined();
+            expect(response.data.USD).not.toBeNaN();
+            expect(response.data.EUR).toBeDefined();
+            expect(response.data.EUR).not.toBeNaN();
+            expect(response.data.BRL).toBeDefined();
+            expect(response.data.BRL).not.toBeNaN();
+        } catch (e) {
+            fail(e);
+        }
+    });
+
+    it('should be able to get the transaction history for an existing wallet address', async function() {
+        try {
+            const walletAddress = '0x589d41feE71B6c972F7AB20e1Fa6EeC11e6C3dF6';
+            const response = await Api.getHistory(walletAddress);
+            expect(response.status).toBe(200);
+            expect(response.data.status).toBe('1');
+            expect(response.data.result).toBeInstanceOf(Array);
+            expect(response.data.result.length).toBeGreaterThan(0);
+        } catch (e) {
+            fail(e);
+        }
+    });
+
+    it('should be able to get an empty transaction history for an unexisting wallet address', async function() {
+        try {
+            const walletAddress = 'notavalidaddress';
+            const response = await Api.getHistory(walletAddress);
+            expect(response.status).toBe(200);
+            expect(response.data.status).toBe('0');
+            expect(response.data.result).toBeInstanceOf(Array);
+            expect(response.data.result.length).toBe(0);
         } catch (e) {
             fail(e);
         }
--- src/common/services/api.js
+++ src/common/services/api.js
@@ -4,3 +4,7 @@ import { Url } from '@common/constants';
 export function getPrice() {
     return axios.get(`${Url.CRYPTO_COMPARE}/data/price?fsym=ETH&tsyms=USD,EUR,BRL`);
 }
+
+export function getHistory(address) {
+    return axios.get(`${Url.ETHERSCAN}?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc`);
+}
