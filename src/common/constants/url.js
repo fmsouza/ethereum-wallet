789643ba49ea9a6ce4d446272d95a3f28b53eee5
Fred Souza <fredericoamsouza@gmail.com>
2017-12-19 22:47:50 -0200

    Changing Etherscan API addresses to use HTTPS

--- src/common/constants/url.js
+++ src/common/constants/url.js
@@ -1,3 +1,3 @@
 export const CRYPTO_COMPARE = 'https://min-api.cryptocompare.com';
 
-export const ETHERSCAN = (process.env.NODE_ENV === 'production') ? 'http://api.etherscan.io/api' : 'http://rinkeby.etherscan.io/api';
+export const ETHERSCAN = (process.env.NODE_ENV === 'production') ? 'https://api.etherscan.io/api' : 'https://rinkeby.etherscan.io/api';
