e6c00026c3e7cb95a137c01e87ae229e76a8163c
Fred Souza <fredericoamsouza@gmail.com>
2017-12-21 00:51:00 -0200

    Implementing TransactionCard layout

--- src/common/styles/colors.js
+++ src/common/styles/colors.js
@@ -4,6 +4,7 @@ export const gray                   = '#808080';
 export const lightGray              = '#CCCCCC';
 export const lightestGray           = '#EFEFEF';
 export const white                  = '#FFFFFF';
+export const lime                   = '#69CA00';
 
 export const primary                = darkGray;
 export const secondary              = white;
@@ -11,3 +12,6 @@ export const secondary              = white;
 export const statusBar              = primary;
 export const defaultBackground      = white;
 export const splashscreenBackground = darkGray;
+
+export const success                = lime;
+export const pending                = gray;
--- src/components/views/WalletExtract/TransactionCard.js
+++ src/components/views/WalletExtract/TransactionCard.js
@@ -7,9 +7,19 @@ import { Wallet as WalletUtils } from '@common/utils';
 export default class TransactionCard extends React.Component {
 
     get isReceiving() {
-        const { transaction: { to }, walletAddress } = this.props;
-        return (to.toLowerCase() === walletAddress.toLowerCase());
+        return this.to.toLowerCase() === this.props.walletAddress.toLowerCase();
+    }
 
+    get isConfirmed() {
+        return this.props.transaction.confirmations > 0;
+    }
+
+    get from() {
+        return this.props.transaction.from;
+    }
+
+    get to() {
+        return this.props.transaction.to;
     }
 
     get iconName() {
@@ -20,6 +30,27 @@ export default class TransactionCard extends React.Component {
         return WalletUtils.formatBalance(this.props.transaction.value);
     }
 
+    get timestamp() {
+        const { timeStamp } = this.props.transaction;
+        const timestamp = Number(timeStamp) * 1000;
+        const date = new Date(timestamp);
+        return `${date.getDay()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+    }
+
+    renderTransactionOperator = () => (
+        <Text
+            style={styles.operatorLabel}
+            ellipsizeMode="tail"
+            numberOfLines={1}
+            children={this.isReceiving ? `From ${this.from}` : `To ${this.to}`} />
+    );
+
+    renderConfirmationStatus() {
+        return this.isConfirmed ?
+            <Icon name="checkmark" color={colors.success} /> :
+            <Icon name="clock" type="ei" color={colors.pending} />
+    }
+
     render() {
         const { transaction } = this.props;
         return (
@@ -28,9 +59,21 @@ export default class TransactionCard extends React.Component {
                     <Icon name={this.iconName} type="fe" />
                 </View>
                 <View style={styles.centerColumn}>
-                    <Text>{transaction.from}</Text>
-                    <Text>{transaction.to}</Text>
-                    <Text>{this.amount}</Text>
+                    {this.renderTransactionOperator()}
+                    <Text>{this.timestamp}</Text>
+                </View>
+                <View style={styles.rightColumn}>
+                    <View style={styles.amountContainer}>
+                        <Text
+                            style={styles.amountLabel}
+                            ellipsizeMode="tail"
+                            numberOfLines={1}
+                            children={this.amount} />
+                        <Text style={styles.fiatLabel}>US$ 0</Text>
+                    </View>
+                    <View style={styles.confirmationsContainer}>
+                        {this.renderConfirmationStatus()}
+                    </View>
                 </View>
             </View>
         );
@@ -59,5 +102,32 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         alignItems: 'flex-start',
         justifyContent: 'center'
+    },
+    operatorLabel: {
+        fontWeight: 'bold',
+        fontSize: measures.fontSizeMedium
+    },
+    rightColumn: {
+        paddingHorizontal: measures.defaultPadding,
+        width: 100,
+        flexDirection: 'row',
+    },
+    amountContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'flex-end'
+    },
+    confirmationsContainer: {
+        marginLeft: measures.defaultMargin,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 20
+    },
+    amountLabel: {
+        fontWeight: 'bold',
+        fontSize: measures.fontSizeMedium
+    },
+    fiatLabel: {
+        fontSize: measures.fontSizeMedium - 4
     }
 });
