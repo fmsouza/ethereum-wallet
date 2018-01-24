808094bcd3a88765617198f1b6ac9da4c4fe1da3
Fred Souza <fredericoamsouza@gmail.com>
2018-01-23 23:27:40 -0200

    Show snackbar when the transaction is confirmed

--- src/common/actions/transactions.js
+++ src/common/actions/transactions.js
@@ -1,10 +1,13 @@
 import ethers from 'ethers';
+import Snackbar from 'react-native-snackbar';
 import { wallet as WalletStore } from '@common/stores';
 import { Transactions as TransactionsService } from '@common/services';
+import { colors } from '@common/styles';
 
 async function waitForTransaction(wallet, txn) {
   txn = await wallet.provider.waitForTransaction(txn.hash);
   WalletStore.moveToHistory(txn);
+  Snackbar.show({ title: 'Transaction confirmed', duration: Snackbar.LENGTH_SHORT });
 }
 
 export async function sendEther(wallet, destination, amount, options) {
