4bb6a78bb1abda7b7d0dd4264b820390129ac0bb
Fred Souza <fredericoamsouza@gmail.com>
2018-03-06 16:47:35 -0300

    Hiding some warnings

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import './src/common/utils/shims';
 import App from './src';
+
+YellowBox.ignoreWarnings([
+  'Warning: componentWillMount',
+  'Warning: componentWillReceiveProps',
+  'Module RCTImageLoader'
+]);
 
 AppRegistry.registerComponent('EthereumWallet', () => App);
