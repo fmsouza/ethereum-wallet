dab09705a5720996aafe1aeb25e0807cb88425e6
Fred Souza <fredericoamsouza@gmail.com>
2018-03-05 22:49:08 -0300

    Changing relative path in the app bootstrap file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
-import '@common/utils/shims';
+import './src/common/utils/shims';
 import App from './src';
 
 AppRegistry.registerComponent('EthereumWallet', () => App);
