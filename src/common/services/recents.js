c79245294ea9c5a0f7ba99957601dc32c7f747ba
Fred Souza <fredericoamsouza@gmail.com>
2018-03-16 00:11:58 -0300

    Creating Recents storage service

---
+++ src/common/services/recents.js
@@ -0,0 +1,11 @@
+import * as StorageService from './storage';
+import { Recents } from '@common/constants';
+
+export async function loadRecentAddresses() {
+    const recents = await StorageService.getItem(Recents.STORAGE_KEY);
+    return recents ? JSON.parse(recents) : [];
+}
+
+export async function saveRecentAddresses(recents) {
+    await StorageService.setItem(Wallet.STORAGE_KEY, JSON.stringify(map));
+}
