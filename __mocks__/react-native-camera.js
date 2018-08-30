ef33fc0175a19cfbf8acfb056253cf892689c4ed
Fred Souza <fredericoamsouza@gmail.com>
2018-08-30 20:34:55 -0300

    Mocking `RNCamera` on jest

---
+++ __mocks__/react-native-camera.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export class RNCamera extends React.Component {
+
+  static Constants = {
+    BarCodeType: 'BarCodeType'
+  };
+
+  render() {
+    return (
+      <View />
+    );
+  }
+}
