fe88b26d719c4af7ab82d0249ce558781e7ff07c
Fred Souza <fredericoamsouza@gmail.com>
2018-08-30 22:59:47 -0300

    Exporting RecentsService

--- src/common/services/index.js
+++ src/common/services/index.js
@@ -1,6 +1,7 @@
 import * as Api from './api';
+import * as Recents from './recents';
 import * as Storage from './storage';
 import * as Transactions from './transactions';
 import * as Wallets from './wallets';
 
-export { Api, Storage, Transactions, Wallets };
+export { Api, Recents, Storage, Transactions, Wallets };
