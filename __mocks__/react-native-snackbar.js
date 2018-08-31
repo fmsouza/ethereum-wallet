3422376b6e51820aa00b8319d38ed988e223eac9
Fred Souza <fredericoamsouza@gmail.com>
2018-08-31 00:00:41 -0300

    Fixing broken tests

---
+++ __mocks__/react-native-snackbar.js
@@ -0,0 +1,6 @@
+export const LENGTH_LONG = 'LENGTH_LONG';
+export const LENGTH_INDEFINITE = 'LENGTH_INDEFINITE';
+export const LENGTH_SHORT = 'LENGTH_SHORT';
+
+
+export function show({ title, duration }) {}
--- src/common/actions/__tests__/recents.js
+++ src/common/actions/__tests__/recents.js
@@ -21,19 +21,19 @@ describe('RecentsActions', () => {
         recentsStoreStub.list = [];
     });
 
-    it('should update the store while loading recents', () => {
+    it('should update the store while loading recents', async () => {
         const address = '0x12345';
         recentsServiceStub.loadRecentAddresses.mockImplementationOnce(() => [address]);
-        Action.loadRecents(recentsStoreStub, recentsServiceStub);
+        await Action.loadRecents(recentsStoreStub, recentsServiceStub);
         expect(recentsStoreStub.isLoading).toHaveBeenCalledTimes(2);
         expect(recentsStoreStub.isLoading).toHaveBeenNthCalledWith(1, true);
         expect(recentsStoreStub.isLoading).toHaveBeenNthCalledWith(2, false);
         expect(recentsStoreStub.loadAddresses).toHaveBeenCalledWith([address])
     });
 
-    it('should save new address to the recents and storage', () => {
+    it('should save new address to the recents and storage', async () => {
         const address = '0x12345';
-        Action.saveAddressToRecents(address, recentsStoreStub, recentsServiceStub);
+        await Action.saveAddressToRecents(address, recentsStoreStub, recentsServiceStub);
         expect(recentsStoreStub.addAddress).toHaveBeenCalledWith(address);
         expect(recentsStoreStub.isLoading).toHaveBeenCalledTimes(2);
         expect(recentsStoreStub.isLoading).toHaveBeenNthCalledWith(1, true);
