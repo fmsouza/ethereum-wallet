6462f8f593cd78edb0e8125a7d92a3d13d382497
Fred Souza <fredericoamsouza@gmail.com>
2018-08-30 23:42:27 -0300

    Functions should be async

--- src/common/actions/recents.js
+++ src/common/actions/recents.js
@@ -1,16 +1,16 @@
 import { Recents as RecentsService } from '@common/services';
 import { recents as RecentsStore } from '@common/stores';
 
-export function loadRecents(store=RecentsStore, service=RecentsService) {
+export async function loadRecents(store=RecentsStore, service=RecentsService) {
     store.isLoading(true);
-    const recents = service.loadRecentAddresses();
+    const recents = await service.loadRecentAddresses();
     store.loadAddresses(recents);
     store.isLoading(false);
 }
 
-export function saveAddressToRecents(address, store=RecentsStore, service=RecentsService) {
+export async function saveAddressToRecents(address, store=RecentsStore, service=RecentsService) {
     store.isLoading(true);
     store.addAddress(address);
-    service.saveRecentAddresses(store.list);
+    await service.saveRecentAddresses(store.list);
     store.isLoading(false);
 }
