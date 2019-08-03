dbf3338738d3c9f64f6a4cbd868347bfb35fa861
Fred Souza <fredericoamsouza@gmail.com>
2019-08-03 13:36:13 -0300

    Fixing skipped tests

--- src/common/actions/__tests__/transactions.js
+++ src/common/actions/__tests__/transactions.js
@@ -10,33 +10,6 @@ describe('TransactionsActions', () => {
 
   beforeEach(() => WalletStore.reset());
 
-  it('`sendEther` should break if the the destination address or the value are invalid', async function() {
-    const mnemonics = WalletUtils.generateMnemonics();
-    const wallet = WalletUtils.loadWalletFromMnemonics(mnemonics);
-    try {
-      await Transactions.sendEther(null, '0x12345', '5.0');
-      fail('should have thrown an Error.');
-    } catch (e) { expect(e.message).toBe('Invalid wallet'); }
-
-    try {
-      await Transactions.sendEther(wallet, null, '5.0');
-      fail('should have thrown an Error.');
-    } catch (e) { expect(e.message).toBe('Invalid destination address'); }
-  });
-
-  it.skip('`sendEther` should send ether to some address when there are funds available', async function() {
-    jest.setTimeout(60000);
-    const wallet = WalletUtils.loadWalletFromPrivateKey(WALLET_PK);
-    const value = '0.002';
-    try {
-      const txn = await Transactions.sendEther(wallet, DESTINATION_ADDRESS, value);
-      expect(txn.from).toBe(wallet.address);
-      expect(txn.to).toBe(DESTINATION_ADDRESS);
-      expect(txn.value.toString()).toBe(ethers.utils.parseEther(value).toString());
-      expect(WalletStore.history.length).toBe(1);
-    } catch (e) { fail(e); }
-  });
-
   it('`sendTransaction` should break if the the wallet or the transaction is invalid', async function() {
     const mnemonics = WalletUtils.generateMnemonics();
     const wallet = WalletUtils.loadWalletFromMnemonics(mnemonics);
@@ -51,7 +24,7 @@ describe('TransactionsActions', () => {
     } catch (e) { expect(e.message).toBe('Invalid destination address'); }
   });
 
-  it.skip('`sendTransaction` should send ether to some address when there are funds available', async function() {
+  it('`sendTransaction` should send ether to some address when there are funds available', async function() {
     jest.setTimeout(60000);
     const wallet = WalletUtils.loadWalletFromPrivateKey(WALLET_PK);
     const value = '0.002';
@@ -61,7 +34,7 @@ describe('TransactionsActions', () => {
       expect(txn.from).toBe(wallet.address);
       expect(txn.to).toBe(DESTINATION_ADDRESS);
       expect(txn.value.toString()).toBe(ethers.utils.parseEther(value).toString());
-      expect(WalletStore.history.length).toBe(1);
+      expect(WalletStore.pendingTransactions.length).toBe(1);
     } catch (e) { fail(e); }
   });
 });
--- src/common/services/__tests__/transaction.js
+++ src/common/services/__tests__/transaction.js
@@ -2,7 +2,7 @@ import { Wallet as WalletUtils, Transaction as TransactionUtils } from '@common/
 import * as Transactions from '../transactions';
 import { ethers } from 'ethers';
 
-describe.skip('TransactionsService', () => {
+describe('TransactionsService', () => {
 
   it('`sendTransaction` should break if the wallet is invalid', async function() {
     const wallet = null;
@@ -66,7 +66,7 @@ describe.skip('TransactionsService', () => {
     try {
       await Transactions.sendTransaction(wallet, txn);
       fail('should have thrown an Error.');
-    } catch (e) { expect(e.message).toBe('insufficient funds for gas * price + value'); }
+    } catch (e) { expect(e.message).toMatch(/insufficient funds/); }
   });
 
   it('`sendTransaction` should send ether to some address when there are funds available', async function() {
@@ -77,33 +77,6 @@ describe.skip('TransactionsService', () => {
     const transaction = TransactionUtils.createTransaction(to, value);
     try {
       const txn = await Transactions.sendTransaction(wallet, transaction);
-      expect(txn.from).toBe(wallet.address);
-      expect(txn.to).toBe(to);
-      expect(txn.value.toString()).toBe(ethers.utils.parseEther(value).toString());
-    } catch (e) { fail(e); }
-  });
-
-  it('`sendEther` should break if the the destination address or the value are invalid', async function() {
-    const mnemonics = WalletUtils.generateMnemonics();
-    const wallet = WalletUtils.loadWalletFromMnemonics(mnemonics);
-    try {
-      await Transactions.sendEther(null, '0x12345', '5.0');
-      fail('should have thrown an Error.');
-    } catch (e) { expect(e.message).toBe('Invalid wallet'); }
-
-    try {
-      await Transactions.sendEther(wallet, null, '5.0');
-      fail('should have thrown an Error.');
-    } catch (e) { expect(e.message).toBe('Invalid destination address'); }
-  });
-
-  it('`sendEther` should send ether to some address when there are funds available', async function() {
-    const pk = '62384683889eae6de8440eb735856f31bb4f17815888f847c8567b3c87f00be8';
-    const wallet = WalletUtils.loadWalletFromPrivateKey(pk);
-    const to = '0x407428BF09ea7Dac2824A64AfE88171041a02b14';
-    const value = '0.002';
-    try {
-      const txn = await Transactions.sendEther(wallet, to, value);
       expect(txn.from).toBe(wallet.address);
       expect(txn.to).toBe(to);
       expect(txn.value.toString()).toBe(ethers.utils.parseEther(value).toString());
